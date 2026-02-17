e1d05687a9e912f505be530198f8d7ba761ad5b5
Jerryyy21 <111446905+Jerryyy21@users.noreply.github.com>
2026-02-17 01:23:04 -0600

    ajuste dashboard y modal funcionando

--- src/app/screens/dashboard-screen/dashboard-screen.ts
+++ src/app/screens/dashboard-screen/dashboard-screen.ts
@@ -6,9 +6,13 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { NewPostModal } from '../../partials/new-post-modal/new-post-modal';
 
 @Component({
   selector: 'app-dashboard-screen',
@@ -25,6 +29,8 @@ import { MatSelectModule } from '@angular/material/select';
     MatFormFieldModule,
     MatInputModule,
     MatSelectModule,
+
+    MatDialogModule,
   ],
   templateUrl: './dashboard-screen.html',
   styleUrl: './dashboard-screen.scss',
@@ -33,10 +39,13 @@ export class DashboardScreen {
   activeSection = 'Inicio';
   activeCategory = 'Todas';
 
+  // ✅ para tu [(ngModel)]
+  searchQuery = '';
+  sortBy: 'recent' | 'popular' | 'comments' = 'recent';
+
   userType = localStorage.getItem('userType') || 'Alumno';
 
-  searchQuery = '';
-  sortBy = 'recent';
+  constructor(private dialog: MatDialog) {}
 
   setSection(section: string) {
     this.activeSection = section;
@@ -44,5 +53,18 @@ export class DashboardScreen {
 
   setCategory(category: string) {
     this.activeCategory = category;
+  }
+
+  openNewPostModal() {
+    const dialogRef = this.dialog.open(NewPostModal, {
+      width: '720px',
+      maxWidth: '92vw',
+      panelClass: 'cf-dialog',
+      autoFocus: false,
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('Modal result:', result);
+    });
   }
 }
